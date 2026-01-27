5abee640e2206fba15a2ecdbc0a1dec4ec3f7f74
floreserpsicologico-lang <floreserpsicologico@gmail.com>
2026-01-27 22:24:43 +0000

    Camibando datos

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+import { createClient } from 'https://oypdcjkutcxvwjajvdlc.supabase.co'
 
 const supabaseUrl = 'https://kwfhwrerevplenhzqoml.supabase.co' 
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3Zmh3cmVyZXZwbGVuaHpxb21sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxMjgxODYsImV4cCI6MjA4NDcwNDE4Nn0.g88luu8ARt0cCeqdsXDL0xLmZBFsqyDqteRLhrbT7sM'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95cGRjamt1dGN4dndqYWp2ZGxjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1NTA4NjksImV4cCI6MjA4NTEyNjg2OX0.ck0javQ16O96xMrDpLAvXLztbCGezYgtWXThYJatO8w'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
